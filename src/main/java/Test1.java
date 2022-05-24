dca4fad9357b3ae224f571397f7a28b3d7ff8511
Lorena <lorenamunozledezma@Lorenas-MacBook-Pro.local>
2022-05-23 21:10:47 -0500

    designTemplateValidation

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Test1 {
 
     @Test
@@ -24,6 +26,23 @@ public class Test1 {
 
 
 
+
+
+    }
+
+    @Test
+    public void designTemplateValidation() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.etsy.com/giftcards/");
+
+        List<WebElement> designTemplateList = driver.findElements(By.xpath("//span[@class='wt-grid__item-xs-4 wt-grid__item-md-4 wt-grid__item-lg-3 wt-grid__item-xl-3 wt-p-xs-0']"));
+        Thread.sleep(3000);
+        for(WebElement designTemp : designTemplateList){
+            Thread.sleep(3000);
+            designTemp.click();
+            Assert.assertTrue(designTemp.isDisplayed());
+        }
 
 
     }
