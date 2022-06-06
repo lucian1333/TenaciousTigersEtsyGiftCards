4b2abcad9ec9bd3d157e7c5bd77cd4b394bc3667
lore-munoz <lore.munoz075@gmail.com>
2022-06-06 13:43:19 -0500

    changes

--- src/main/java/com/test/etsy/pages/DeliveryMethodPage.java
+++ src/main/java/com/test/etsy/pages/DeliveryMethodPage.java
@@ -1,2 +1,73 @@
-package com.test.etsy.pages;public class DeliveryMethodPage {
+package com.test.etsy.pages;
+
+import com.test.etsy.TestBase;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class DeliveryMethodPage extends TestBase {
+
+    public DeliveryMethodPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//fieldset[@data-selector='giftcards-editor-medium-picker']//div//input")
+    List<WebElement> radioButtons;
+
+    @FindBy(xpath = "//div[@class='wt-radio']//label[@for='medium-email']") //--> this is label
+    WebElement emailToRecipientMessage;
+
+    @FindBy(xpath = "//input[@id='medium-email']") // --> this is radio button
+    WebElement emailToRecipientButton;
+
+    @FindBy(xpath = "//div[@class='wt-radio']//label[@for='medium-print']") //--> this is label
+    WebElement emailToSelfMessage;
+
+    @FindBy(xpath = "//input[@id='medium-print']") // --> this is radio button
+    WebElement emailToSelfButton;
+
+    @FindBy(xpath = "//input[@id='recipient_name']")
+    WebElement recipientName;
+
+    @FindBy(xpath = "//textarea[@placeholder='Gift message (optional)']")
+    WebElement optionalMessage;
+
+    @FindBy(xpath = "//input[@name='sender_name']")
+    WebElement sender;
+
+    @FindBy(id = "recipient_email")
+    WebElement recipientEmail;
+
+    @FindBy(xpath = "//button[@class='wt-btn wt-btn--primary wt-width-full']")
+    WebElement addToCartButton;
+
+    public void emailToRecipientSelection() throws InterruptedException {
+
+
+        for(WebElement button : radioButtons){
+            button.click();
+            if(emailToRecipientButton.isSelected()){
+                Assert.assertTrue(recipientName.isDisplayed());
+                Assert.assertTrue(recipientEmail.isDisplayed());
+                Assert.assertTrue(optionalMessage.isDisplayed());
+                Assert.assertTrue(sender.isDisplayed());
+                break;
+            }else if(emailToSelfButton.isSelected()){
+                Assert.assertTrue(recipientName.isDisplayed());
+                Assert.assertTrue(optionalMessage.isDisplayed());
+                Assert.assertTrue(sender.isDisplayed());
+            }
+        }
+
+
+    }
+
+
+
+
+
 }
--- src/main/java/com/test/etsy/test/DeliveryMethodTest.java
+++ src/main/java/com/test/etsy/test/DeliveryMethodTest.java
@@ -1,2 +1,69 @@
-package com.test.etsy.test;public class DeliveryMethodTest {
+package com.test.etsy.test;
+
+import com.test.etsy.TestBase;
+import com.test.etsy.pages.DeliveryMethodPage;
+import com.test.etsy.pages.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DeliveryMethodTest extends TestBase {
+
+    /*
+    TC-01 When I first access the gift card page,
+    the radio button for Email to Recipient is selected
+
+    TC-02 I can instead choose Email to you (printable)
+    and go back to Email to recipient
+
+    TC-03 The option that is selected shows a bolded padding
+     inside the radio button circle
+
+    TC-04 If I choose email to recipient, the following
+    fields are shown: To: Recipient's Name (required);
+    Recipient's Email (required); Gift message (optional);
+    From: Sender's Name (required). If I try to submit the
+    form without filling in the required fields,
+    I receive an error
+
+    TC-05 If I choose email to you (printable), the
+    following fields are shown: To: Recipient's Name
+    (required); Gift message (optional); From:
+     Sender's Name (required)
+
+    TC-06 If I add the item to my cart, the delivery method
+     I selected is preserved
+
+    TC-07 If I choose Email to recipient and purchase the
+    gift card, an email is sent to the address in the
+    Recipient's email field.
+
+    TC-08 If I choose Email to you (printable) and purchase
+    the gift card, an email is sent to the address in the
+    profile of the shopper.
+     */
+
+    public WebDriver driver;
+
+    @BeforeMethod // it will run before all of the other test annotations
+    public void setUp() throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.etsy.com/");
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void validateDeliveryMethodSelection() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        homePage.ClickShopGiftCards(driver);
+        DeliveryMethodPage deliveryMethodPage = new DeliveryMethodPage(driver);
+        deliveryMethodPage.emailToRecipientSelection();
+
+    }
+
+
 }
